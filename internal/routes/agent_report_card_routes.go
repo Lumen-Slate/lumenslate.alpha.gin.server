29f16a3aa8d6253dd8c6063185410ee282039f37
Kartikey <kartikeysingh428@gmail.com>
2025-07-25 22:37:37 +0530

    POST endpoints for assignment result and agent report card

--- internal/routes/agent_report_card_routes.go
+++ internal/routes/agent_report_card_routes.go
@@ -13,7 +13,7 @@ func SetupAgentReportCardRoutes(router *gin.Engine) {
 		api.GET("/agent-report-cards", controller.GetAllAgentReportCards)
 		api.GET("/agent-report-cards/:id", controller.GetAgentReportCardByID)
 		api.GET("/agent-report-cards/student/:studentId", controller.GetAgentReportCardsByStudentID)
-		// api.POST("/agent-report-cards", controller.CreateAgentReportCard)
+		api.POST("/agent-report-cards", controller.CreateAgentReportCard)
 		api.PUT("/agent-report-cards/:id", controller.UpdateAgentReportCard)
 		api.DELETE("/agent-report-cards/:id", controller.DeleteAgentReportCard)
 	}
