0084c9fa418b57e8e7f7740b5f391b094d1644b5
SHUBHAM SHARMA <shubham27.sharma03@gmail.com>
2025-09-07 16:34:40 +0530

    Remove leading slashes from user route paths
    Updated user route definitions to remove leading slashes, aligning with Gin's routing conventions and preventing potential route mismatches.

--- internal/routes/user_routes.go
+++ internal/routes/user_routes.go
@@ -9,11 +9,11 @@ import (
 func RegisterUserRoutes(router *gin.RouterGroup) {
 	user := router.Group("/users")
 	{
-		user.POST("/", controller.CreateUser)
-		user.GET("/:id", controller.GetUser)
-		user.PUT("/:id", controller.UpdateUser)
-		user.PATCH("/:id", controller.PatchUser)
-		user.DELETE("/:id", controller.DeleteUser)
-		user.GET("/", controller.ListUsers)
+		user.POST("", controller.CreateUser)
+		user.GET(":id", controller.GetUser)
+		user.PUT(":id", controller.UpdateUser)
+		user.PATCH(":id", controller.PatchUser)
+		user.DELETE(":id", controller.DeleteUser)
+		user.GET("", controller.ListUsers)
 	}
 }
